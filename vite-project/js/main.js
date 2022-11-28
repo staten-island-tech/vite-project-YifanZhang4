51f29272a93f47d948b5fde10118d2f3a9514f92
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2022-11-28 13:14:22 -0500

    started

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,21 +1,14 @@
 import "../styles/style.css";
 
-import { setupCounter } from "../counter.js";
+const DOMSelectors = {
+  theme: document.getElementById("theme"),
+};
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
- 
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
+DOMSelectors.theme.addEventListener("click", function () {
+  theme.textContent = "Dark Theme";
+  const addCSS = (css) =>
+    (document.body.appendChild(document.createElement("style")).innerHTML =
+      css);
+  addCSS("body{ background-color: #3b3b3b;");
+  addCSS("#header{ color: #dbdbdb;");
+});
