84badabffece957b3fe8c07f482547910d38756b
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2022-11-30 20:10:58 -0500

    dsweargerfd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,14 +4,16 @@ import "aos/dist/aos.css";
 AOS.init();
 import { menu } from "./menu";
 const DOMSelectors = {
-  theme: document.getElementById("theme"),
+  themeBtn: document.querySelector(".themebtn"),
 };
 
-DOMSelectors.theme.addEventListener("click", function () {
-  theme.textContent = "Dark Theme";
-  const addCSS = (css) =>
-    (document.body.appendChild(document.createElement("style")).innerHTML =
-      css);
-  addCSS("body{ background-color: var(--primary);");
-  addCSS("#header{ color: var(--secondary);");
+DOMSelectors.themeBtn.addEventListener("click", function () {
+  if(document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+
 });
