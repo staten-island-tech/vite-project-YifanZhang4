97b210ab2afa25972b824001159c774329fca4aa
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2022-12-02 13:14:53 -0500

    /lk.,k,jhgfc jnm

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,7 +1,54 @@
 const menu = [
   {
-    name: "Primogems",
-    Conversion_Needed: true,
+    name: "Original Milk Tea",
+    inStock: false,
+    MilkTea: true,
+    img: "https://brewedleaflove.com/wp-content/uploads/2019/07/bubble-tea-recipe-scaled.jpg",
+  },
+  {
+    name: "Thai Milk Tea",
+    inStock: true,
+    MilkTea: true,
+  },
+  {
+    name: "Passionfruit Green Tea",
+    inStock: true,
+    MilkTea: false,
+  },
+  {
+    name: "Strawberry Green Tea",
+    inStock: false,
+    MilkTea: false,
+  },
+  {
+    name: "Black Sugar Oolong Tea",
+    inStock: false,
+    MilkTea: false,
+  },
+  {
+    name: "Brown Sugar Milk Tea",
+    inStock: false,
+    MilkTea: true,
+  },
+  {
+    name: "Matcha Milk Tea",
+    inStock: true,
+    MilkTea: true,
+  },
+  {
+    name: "Taro Milk Tea",
+    inStock: true,
+    MilkTea: true,
+  },
+  {
+    name: "Lychee Green Tea",
+    inStock: true,
+    MilkTea: false,
+  },
+  {
+    name: "Oreo Milk Tea",
+    inStock: false,
+    MilkTea: true,
   },
 ];
 
